ab02b0d0f4ba0f010980d3ea624ec63c131c47db
Chris Carrington <chris@megafarad.com>
2019-10-03 16:54:35 -0700

    Just for "fun"

--- src/main/java/com/megafarad/SortUtils.java
+++ src/main/java/com/megafarad/SortUtils.java
@@ -1,6 +1,11 @@
 package com.megafarad;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SortUtils {
+
+    public static Logger logger = LoggerFactory.getLogger(SortUtils.class);
 
     public static <E extends Comparable<E>> void bubbleSort(E[] arrayToSort) {
         int numElements = arrayToSort.length;
@@ -8,12 +13,52 @@ public class SortUtils {
         for (int i = 0; i < numElements - 1; i++) {
             for (int j = 0; j < numElements - i - 1; j++) {
                 if (arrayToSort[j].compareTo(arrayToSort[j + 1]) > 0) {
-                    E tmp = arrayToSort[j];
-                    arrayToSort[j] = arrayToSort[j + 1];
-                    arrayToSort[j + 1] = tmp;
+                    swap(arrayToSort, j, j + 1);
                 }
             }
         }
     }
+
+    public static <E extends Comparable<E>> void quickSort(E[] arrayToSort) {
+        quickSort(arrayToSort, 0, arrayToSort.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void quickSort(E[] arrayToSort, int leftIdx, int rightIdx) {
+        logger.info("Calling quicksort on array: {} with left: {} and right: {}", arrayToSort, leftIdx, rightIdx);
+        if (leftIdx >= rightIdx) {
+            return;
+        }
+        E pivot = arrayToSort[(leftIdx + rightIdx) / 2];
+        logger.info("pivot: {}", pivot);
+        int newPivotPoint = partition(arrayToSort, leftIdx, rightIdx, pivot);
+        logger.info("new pivot point: {}", newPivotPoint);
+        quickSort(arrayToSort, leftIdx, newPivotPoint - 1);
+        quickSort(arrayToSort, newPivotPoint, rightIdx);
+    }
+
+    private static <E extends Comparable<E>> int partition(E[] array, int leftIdx, int rightIdx, E pivot) {
+        while (leftIdx <= rightIdx) {
+            while (array[leftIdx].compareTo(pivot) < 0) {
+                leftIdx++;
+            }
+            while (array[rightIdx].compareTo(pivot) > 0) {
+                rightIdx--;
+            }
+            if (leftIdx <= rightIdx) {
+                swap(array, leftIdx, rightIdx);
+                leftIdx++;
+                rightIdx--;
+            }
+        }
+        return leftIdx;
+    }
+
+
+    private static <E extends Comparable<E>> void swap(E[] array, int indexA, int indexB) {
+        E tmp = array[indexA];
+        array[indexA] = array[indexB];
+        array[indexB] = tmp;
+    }
+
 
 }
--- src/test/java/com/megafarad/SortUtilsTest.java
+++ src/test/java/com/megafarad/SortUtilsTest.java
@@ -16,4 +16,13 @@ class SortUtilsTest {
         System.out.println(Arrays.toString(sortableArray));
         assertArrayEquals(expectedArray, sortableArray);
     }
+
+    @Test
+    void quickSort() {
+        Integer[] sortableArray = new Integer[]{9,1,2,6,8,3,5,4,7,0};
+        Integer[] expectedArray = new Integer[]{0,1,2,3,4,5,6,7,8,9};
+        SortUtils.quickSort(sortableArray);
+        System.out.println(Arrays.toString(sortableArray));
+        assertArrayEquals(expectedArray, sortableArray);
+    }
 }
